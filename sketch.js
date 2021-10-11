afe8ea248c0c0d3dd0db27892cff844230833a40
Voyager <levan.miqatadze003@gmail.com>
2021-10-11 15:55:41 +0400

    added border wrap around

--- sketch.js
+++ sketch.js
@@ -1,50 +1,48 @@
-function makearray(cols,rows){
-  let array = new Array(rows).fill(0).map(() => new Array(cols).fill(0));
+function makearray(rows,cols){
+  let array
+  array = new Array(cols).fill(0)
+  for(let i=0;i<cols;i++){
+    array[i] = new Array(rows).fill(0)
+  } 
   return array;
 }
-function randomgrid(grid,cols,rows){
-    for(let i = 0; i<rows;i++){
-      for(let j =0;j<cols;j++){
+function mod(n, m) {
+  if(m!=0){
+  return ((n % m) + m) % m;
+  }
+  else{return }
+}
+function randomgrid(grid,rows,cols){
+    for(let i = 0; i<cols;i++){
+      for(let j =0;j<rows;j++){
         grid[i][j] = Math.floor(random(2))
     }
   }
   return grid
 }
-function neighbor(grid,col,row){
-  let counter = 0;
-  for(let i =-1;i<2;i++){
-    for(let j = -1;j<2;j++){
-      let newI = col + i;
-      let newJ = row + j;
-      counter += grid[newI][newJ]
-    }
-  }
-  counter -= grid[col][row]
-  return counter;
-}
+
 function generategeneration(grid,cols,rows,nextgen){
-  for(let i = 1; i<rows-1;i++){
-    for(let j =1;j<cols-1;j++){
-      if(grid[i][j] == 1 && neighbor(grid,i,j)<2){
+  for(let i= 0; i<cols;i++){
+    for(let j=0;j<rows;j++){
+      let p = grid[i][j]
+      let count = neighbor(grid,i,j)
+      if(p == 1 && (count<2 || count >3)){
         nextgen[i][j] =0;
       }
-      if(grid[i][j] == 1 && (neighbor(grid,i,j) == 2 || neighbor(grid,i,j) == 3)){
-        nextgen[i][j] = 1;
+      if(p == 1 && (count == 2 || count == 3)){
+        nextgen[i][j] = p;
       }
-      if(grid[i][j] == 1 && neighbor(grid,i,j)>3){
-        nextgen[i][j] =0;
-      }
-      if(grid[i][j] == 0 && neighbor(grid,i,j)==3){
+      if(p == 0 && count==3){
         nextgen[i][j] =1;
-      }      
+      }
     }
   }
-  
+
   return nextgen;
 }
 function show(grid,cols,rows,resolution){
-  for(let i = 0; i<rows;i++){
-    for(let j =0;j<cols;j++){
+  for(let i = 0; i<cols;i++){
+    for(let j =0;j<rows;j++){
       square(i*resolution,j*resolution,resolution)
       if(grid[i][j] == 1){
         fill(0)
@@ -52,20 +50,34 @@ function show(grid,cols,rows,resolution){
     }
   }
 }
-const rows = 50;
-const cols = 50;
+const rows = 70;
+const cols = 70;
 let grid;
 function setup() {
-   grid = makearray(cols,rows);
-   grid = randomgrid(grid,cols,rows)
+   grid = makearray(rows,cols);
+   grid = randomgrid(grid,rows,cols);
   createCanvas(400, 400);
 }
-
+ 
 function draw() {
+  ful
   background(220);
   stroke(100)
-  let nextgen = makearray(cols,rows)
+  let nextgen = makearray(rows,cols)
   const resolution = width/rows;
   show(grid,cols,rows,resolution)
   grid = generategeneration(grid,cols,rows,nextgen);
+}
+
+function neighbor(grid,col,row){
+  let counter = 0;
+  for(let i =-1;i<2;i++){
+    for(let j = -1;j<2;j++){
+       let newI = (col+j+cols)%cols;
+       let newJ = (row+i+rows)%rows;
+      counter += grid[newI][newJ]
+    }
+  }
+  counter -= grid[col][row]
+  return counter;
 }
